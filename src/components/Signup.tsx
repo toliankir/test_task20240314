2b3670ceca95ea704654e0d1b1e8f2fdc7b15258
Anatolii <toliankir@gmail.com>
2024-03-17 21:56:43 +0100

    Style changes

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -70,7 +70,7 @@ function Signup() {
             <input
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              type="text"
+              type="password"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="password" required />
           </div>
